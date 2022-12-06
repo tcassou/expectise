29425efc0b83b35db81c6b6114f0a4f769b20563
Thomas Cassou <thomas.cassou@pennylane.com>
2022-12-06 23:13:23 +0100

    update docs and clarify exception messages

--- expectise/expect.py
+++ expectise/expect.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Type
+
 from .diff import Diff
 from .exceptions import EnvironmentError
 from .exceptions import ExpectationError
@@ -6,7 +14,7 @@ from .exceptions import ExpectationError
 
 class Expect(object):
     """
-    Mocking function calls for Unit tests purposes, refer to the README for more details and examples.
+    Mocking function calls for unit tests purposes, refer to the README for more details and examples.
     """
 
     # Dict of classes with at least one method mocked, shared between Expect instances: {'class_name': class}
@@ -25,19 +33,13 @@ class Expect(object):
     # }
     method_h = {}
 
-    def __init__(self, class_name):
-        """Init
-
-        :param str class_name: name of the class
-
-        :return: None
-        """
+    def __init__(self, class_name: str) -> None:
         if class_name not in Expect.class_h:
             raise EnvironmentError(
                 f"""
                 No method of class `{class_name}` is mocked, so this instantiation is not allowed.
                 Check that the right environment variable is set, and that the right methods from `{class_name}`
-                are decorated with `@mock_if(your_env_variable_name, your_env_variable_value)`.
+                are decorated with `@mock_if` decorators or standalone `mock` statements.
                 """
             )
         self.klass = Expect.class_h[class_name]
@@ -48,21 +50,15 @@ class Expect(object):
         self.decorator_names = ["raise", "return", "with_args", "classmethod", "staticmethod"]  # Order matters!
         self.decorators = {decorator_name: None for decorator_name in self.decorator_names}
 
-    def get_decoration(self):
-        """Get the right decoration of the original method, depending on the number of calls already performed.
-
-        :return: the right decorated method
-        :rtype: callable object
-        """
+    def get_decoration(self) -> Callable:
+        """Get the right decoration of the original method, depending on the number of calls already performed."""
         call_idx = Expect.method_h[(self.klass.__name__, self.method_name)]["performed"] - 1
         return Expect.method_h[(self.klass.__name__, self.method_name)]["decorators"][call_idx]
 
-    def override_method(self):
-        """Override the original method in order to pick the right decorated method and apply it instead.
-        Check that the method is called (and the right number of times) is done here.
-
-        :return: a decorated method
-        :rtype: callable object
+    def override_method(self) -> Callable:
+        """
+        Return the appropriate override of the mocked method to be applied during tests, according to Expect statements.
+        This override includes checks on whether the method is called, and the right number of times.
         """
 
         def func(*args, **kwargs):
@@ -77,14 +73,8 @@ class Expect(object):
             return classmethod(func)
         return func
 
-    def is_decorated(self, method):
-        """Checking for existing decorators around the target method: classmethod or staticmethod.
-
-        :param callable object method: method to check
-
-        :return: True if the method is decorated, False otherwise
-        :rtype: bool
-        """
+    def is_decorated(self, method: Callable) -> bool:
+        """Checking for existing decorators around the target method: classmethod or staticmethod."""
         if isinstance(method, classmethod):
             self.decorators["classmethod"] = classmethod
             self.is_classmethod = True
@@ -93,22 +83,17 @@ class Expect(object):
             self.is_staticmethod = True
         return self.is_classmethod or self.is_staticmethod
 
-    def decorate(self):
-        """Iteratively apply active decorators to the target method.
-
-        :return: decorated method
-        :rtype: method
-        """
+    def decorate(self) -> Callable:
+        """Iteratively apply active decorators to the target method."""
         method = self.method.__func__ if self.is_decorated(self.method) else self.method
         for decorator_name in self.decorator_names:
             decorator = self.decorators[decorator_name]
             method = decorator(method) if decorator else method
         return method
 
-    def mark_received(self):
-        """Mark decorated method as called once more, and raise exception if it is called more times than expected.
-
-        :return: None
+    def mark_received(self) -> None:
+        """
+        Mark the decorated method as called once more, and raise an exception if it is called more times than expected.
         """
         Expect.method_h[(self.klass.__name__, self.method_name)]["performed"] += 1
         perf = Expect.method_h[(self.klass.__name__, self.method_name)]["performed"]
@@ -118,14 +103,8 @@ class Expect(object):
                 "{}.{} is expected to be called {} time(s) only.".format(self.klass.__name__, self.method_name, exp)
             )
 
-    def to_receive(self, method_name):
-        """Decorating a method to check that it is called.
-
-        :param str method_name: name of the method
-
-        :return: Expect object
-        :rtype: Expect
-        """
+    def to_receive(self, method_name: str) -> Expect:
+        """Decorate the mocked method to check that it is called."""
         key = (self.klass.__name__, method_name)
         self.method_name = method_name
         self.method = Expect.method_h[key]["method"]
@@ -133,7 +112,8 @@ class Expect(object):
             raise EnvironmentError(
                 f"""
                 Method `{method_name}` from class `{self.klass.__name__}` is not mocked.
-                Decorate it with @mock_if(your_env_variable_name, your_env_variable_value) to be able to mock its calls.
+                Decorate it with @mock_if(your_env_variable_name, your_env_variable_value), or through
+                standalone mock() statements to be able to mock its calls.
                 """
             )
         else:
@@ -147,14 +127,8 @@ class Expect(object):
         setattr(self.klass, self.method_name, self.override_method())
         return self
 
-    def assert_arguments(self, func_args, func_kwargs):
-        """Asserting equality of arguments.
-
-        :param [object] func_args: list of passed arguments
-        :param {string: object} func_kwargs: dict of passed keyword arguments
-
-        :return: None
-        """
+    def assert_arguments(self, func_args: List[Any], func_kwargs: Dict[Any, Any]) -> None:
+        """Asserting equality of method arguments."""
         call_idx = Expect.method_h[(self.klass.__name__, self.method_name)]["performed"] - 1
         args, kwargs = Expect.method_h[(self.klass.__name__, self.method_name)]["with_args"][call_idx]
 
@@ -164,14 +138,8 @@ class Expect(object):
         if kwargs != func_kwargs:
             raise ExpectationError(msg.format("keyword") + Diff.print(kwargs, func_kwargs))
 
-    def with_args_decorator(self, method):
-        """Decorator to check passed argument.
-
-        :param callable object method: method to decorate
-
-        :return: decorated method
-        :rtype: method
-        """
+    def with_args_decorator(self, method: Callable) -> Callable:
+        """Decorator to check passed argument."""
 
         def instance_func(obj, *func_args, **func_kwargs):
             self.assert_arguments(func_args, func_kwargs)
@@ -183,15 +151,8 @@ class Expect(object):
 
         return static_func if self.is_staticmethod else instance_func
 
-    def with_args(self, *args, **kwargs):
-        """Decorating a method to check it is called with the desired arguments.
-
-        :param [object] args: list of arguments
-        :param {string: object} kwargs: dict of keyword arguments
-
-        :return: Expect object
-        :rtype: Expect
-        """
+    def with_args(self, *args, **kwargs) -> Expect:
+        """Decorating a method to check it is called with the desired arguments."""
         if self.method is None:
             raise ExpectationError("Expect error: calling .with_args() without prior call of to_receive()")
         self.decorators["with_args"] = self.with_args_decorator
@@ -199,37 +160,21 @@ class Expect(object):
         Expect.method_h[(self.klass.__name__, self.method_name)]["decorators"][-1] = self.decorate()
         return self
 
-    def get_output(self):
-        """Get the right output to return when .and_return() decoration is used.
-
-        :return: output
-        :rtype: object
-        """
+    def get_output(self) -> Any:
+        """Get the right output to return when .and_return() decoration is used."""
         call_idx = Expect.method_h[(self.klass.__name__, self.method_name)]["performed"] - 1
         return Expect.method_h[(self.klass.__name__, self.method_name)]["return"][call_idx]
 
-    def return_decorator(self, method):
-        """Decorator to modify objects returned.
-
-        :param callable object method: method to decorate
-
-        :return: decorated method
-        :rtype: method
-        """
+    def return_decorator(self, method: Callable) -> Callable:
+        """Decorator to modify objects returned."""
 
         def func(*func_args, **func_kwargs):
             return self.get_output()
 
         return func
 
-    def and_return(self, output):
-        """Overwrite a method to output the object passed as argument.
-
-        :param object output: the desired output
-
-        :return: Expect object
-        :rtype: Expect
-        """
+    def and_return(self, output: Any) -> Expect:
+        """Overwrite a method to output the object passed as argument."""
         if self.method is None:
             raise ExpectationError("Expect error: calling and_return() without prior call of to_receive()")
         self.decorators["return"] = self.return_decorator
@@ -237,28 +182,16 @@ class Expect(object):
         Expect.method_h[(self.klass.__name__, self.method_name)]["decorators"][-1] = self.decorate()
         return self
 
-    def raise_decorator(self, method):
-        """Decorator to raise errors.
-
-        :param callable object method: method to decorate
-
-        :return: decorated method
-        :rtype: method
-        """
+    def raise_decorator(self, method: Callable) -> Callable:
+        """Decorator to raise errors."""
 
         def func(*func_args, **func_kwargs):
             raise self.get_output()
 
         return func
 
-    def and_raise(self, error):
-        """Overwrite a method to raise an error when called.
-
-        :param Exception error: the error to raise
-
-        :return: Expect object
-        :rtype: Expect
-        """
+    def and_raise(self, error: Exception) -> Expect:
+        """Overwrite a method to raise an error when called."""
         if self.method is None:
             raise ExpectationError("Expect error: calling and_raise() without prior call of to_receive()")
         self.decorators["raise"] = self.raise_decorator
@@ -267,20 +200,11 @@ class Expect(object):
         return self
 
     @classmethod
-    def set_up(cls, klass, method_name, surrogate):
+    def set_up(cls, klass: Type, method_name: str, surrogate: Callable) -> None:
         """
         Set up the `Expect` context and replace the method being mocked with a surrogate function.
-        This method is called at interpretation time as `mock_if` statements are being resolved, and at the end
-        of each unit test to start the next one with a clean context.
-
-        :param klass: class object owning the mocked method
-        :type klass: object
-        :param method_name: name of the mocked method
-        :type method_name: str
-        :param surrogate: the method to use as surrogate for the method being mocked
-        :type surrogate: callable
-
-        :return: None
+        This method is called at interpretation time when `@mock_if` decorators are being resolved, when explictly
+        performing mock(...) statements, and at the end of each unit test to start the next one with a clean context.
         """
         # When called from `mock_if` decorating statement, adding classes to the list of altered objects
         if klass.__name__ not in cls.class_h:
@@ -299,10 +223,8 @@ class Expect(object):
         setattr(klass, method_name, surrogate)
 
     @classmethod
-    def tear_down(cls):
-        """
-        Check for any method called less times than expected, and raise an AssertionError is any is found.
-        """
+    def tear_down(cls) -> None:
+        """Check for any method called less times than expected, and raise an AssertionError is any is found."""
         message = ""
         for (class_name, method_name), args in cls.method_h.items():
             remain = args["expected"] - args["performed"]
