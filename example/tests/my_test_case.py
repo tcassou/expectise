d52c5e169bce69f4a04fc88c72bb4681921ca8d1
Thomas Cassou <thomas.cassou@pennylane.com>
2022-12-06 18:12:47 +0100

    extract surrogate function in mock_if and switch tests to pytest

--- example/tests/my_test_case.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from unittest import TestCase
-
-from expectise import Expect
-
-
-class MyTestCase(TestCase):
-    def tearDown(self):
-        Expect.tear_down()
--- expectise/mock_if.py
+++ expectise/mock_if.py
@@ -3,51 +3,35 @@ import os
 from .exceptions import EnvironmentError
 from .expect import Expect
 
+from typing import Callable
+from typing import Type
 
-def mock_if(env_name, env_value):
+
+def mock_if(env_name: str, env_value: str) -> Type:
     """
     Decorator to identify which methods should be mocked, only in case the right environment variable is set to the
-    right value (for example, ENV=test).
+    right value. Example:
 
-    :param env_name: name of the environment variable to inspect
-    :type env_name: str
-    :param env_value: value to compare the environment variable env_name with
-    :type env_value: str
-
-    :return: the decorator class that implements the mocking logic
-    :rtype: MockDecorator
+        mock_if("ENV", "test")
+        def foo(...)
+            pass
     """
 
     class MockDecorator:
-        def __init__(self, method):
+        def __init__(self, method: Callable) -> None:
             """
             Decorator class, that takes as input the method to be mocked:
             * if the environment conditions are met, the method is marked as to be mocked for later calls
             * if not, the method is left unchanged.
-
-            :param method: the method to mock in test environment
-            :type method: callable
-
-            :return: None
             """
             self.method = method
-            self.is_classmethod = isinstance(method, classmethod)
-            self.is_staticmethod = isinstance(method, staticmethod)
             self.owner = None
-            self.name = method.__func__.__name__ if self.is_classmethod or self.is_staticmethod else method.__name__
 
-        def __set_name__(self, owner, name):
+        def __set_name__(self, owner: Type, name: str) -> None:
             """
             At interpretation time when the MockDecorator object is created, the surrounding class is not created yet.
             As soon as it is, this `__set_name__` method is called, which gives us a way to know and record the class or
             object that owns the method.
-
-            :param owner: the class or object owning the method
-            :type owner: object
-            :param name: the name of the method
-            :type name: str
-
-            :return: None
             """
             self.owner = owner
             # In any environment that is not our test environment, we do not interfere with the method definition.
@@ -57,29 +41,30 @@ def mock_if(env_name, env_value):
 
             # In test environment, the mocked methods are replaced by placeholders that raise errors if called without
             # the prior use of an `Expect` statement to define how they should behave during tests.
-            Expect.set_up(owner, name, self.surrogate())
-
-        def surrogate(self):
-            """
-            Define a surrogate function that will replace any mocked method, so that any call to the method raises an
-            error if not preceded by the appropriate `Expect` statement.
-
-            :return: the function, possibly wrapped as a staticmethod or classmethod
-            :rtype: callable
-            """
-
-            def func(*args, **kwargs):
-                raise EnvironmentError(
-                    f"""
-                    Method `{self.name}` from class `{self.owner.__name__}` is mocked when {env_name}={env_value},
-                    and will raise errors if called without using an `Expect` statement to define its mocked behavior.
-                    """
-                )
-
-            if self.is_staticmethod:
-                return staticmethod(func)
-            elif self.is_classmethod:
-                return classmethod(func)
-            return func
+            Expect.set_up(owner, name, surrogate(owner, self.method, env_name, env_value))
 
     return MockDecorator
+
+
+def surrogate(klass: type, method: Callable, env_name: str, env_value: str) -> Callable:
+    """
+    Define a surrogate function that will replace any mocked method, so that any call to the method raises an
+    error if not preceded by the appropriate `Expect` statement.
+    """
+    is_classmethod = isinstance(method, classmethod)
+    is_staticmethod = isinstance(method, staticmethod)
+    method_name = method.__func__.__name__ if is_classmethod or is_staticmethod else method.__name__
+
+    def func(*args, **kwargs):
+        raise EnvironmentError(
+            f"""
+            Method `{method_name}` from class `{klass.__name__}` is mocked when {env_name}={env_value},
+            and will raise errors if called without using an `Expect` statement to define its mocked behavior.
+            """
+        )
+
+    if is_classmethod:
+        return classmethod(func)
+    if is_staticmethod:
+        return staticmethod(func)
+    return func
